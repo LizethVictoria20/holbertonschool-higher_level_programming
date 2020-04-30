58768541b1f4b89586314607c573a0afe13234c3
LizethVictoria20 <lizvictoria754@gmail.com>
2020-04-30 17:41:29 +0000

    Update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 from sys import argv
 if __name__ == "__main__":
     argc = len(argv)
-    if argc < 2:
+    if argc == 1:
         print("{} argument. ".format(argc - 1))
-    if argc >= 2:
+    elif argc >= 2:
         print("{} argument: ".format(argc - 1))
         for i in range(1, argc):
             print("{}: {} ".format(i, argv[i]))
