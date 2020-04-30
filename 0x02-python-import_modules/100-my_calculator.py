eb860cae8f338f379f87f2488b0abc98d752e90e
LizethVictoria20 <lizvictoria754@gmail.com>
2020-04-30 20:17:26 +0000

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,21 +4,19 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
+        print('Usage: {:s} <a> <operator> <b>'.format(sys.argv[0]))
         sys.exit(1)
-
     if sys.argv[2] not in ['+', '-', '*', '/']:
-        print("Unknown operator. Available operators: +, -, * and /")
+        print('Unknown operator. Available operators: +, -, * and /')
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-
-    if sys.argv[2] == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    if sys.argv[2] == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    if sys.argv[2] == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    if sys.argv[2] == "/":
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    if sys.argv[2] == '+':
+        print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    elif sys.argv[2] == '-':
+        print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    elif sys.argv[2] == '*':
+        print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    else:
+        print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
