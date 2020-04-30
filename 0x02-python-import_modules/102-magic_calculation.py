9bb3fa200572d51cfb8db98b5e1c4b4ab4a22f2c
LizethVictoria20 <lizvictoria754@gmail.com>
2020-04-30 22:18:44 +0000

    Magic calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import
-from magic_calculation_102 import add, sub, mul, div
+from magic_calculation_102 import add, sub
 def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
