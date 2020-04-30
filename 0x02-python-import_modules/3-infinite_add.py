802aaaea3cfef850f8e5957cb68b3274293754d1
LizethVictoria20 <lizvictoria754@gmail.com>
2020-04-30 16:53:41 +0000

    Add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    argc = len(argv)
+    add = 0
+    for i in range(1, argc):
+        add += int(argv[i])
+    print("{}".format(add))
