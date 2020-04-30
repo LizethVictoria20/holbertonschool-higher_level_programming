fca8e7c4f2dcc329b37ed734001292f223e373be
LizethVictoria20 <lizvictoria754@gmail.com>
2020-04-30 21:39:11 +0000

    Update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print('Usage: {:s} <a> <operator> <b>'.format(sys.argv[0]))
-        sys.exit(1)
-    if sys.argv[2] not in ['+', '-', '*', '/']:
-        print('Unknown operator. Available operators: +, -, * and /')
-        sys.exit(1)
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if sys.argv[2] == '+':
-        print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-    elif sys.argv[2] == '-':
-        print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-    elif sys.argv[2] == '*':
-        print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    else:
-        print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+    if len(argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    op = argv[2]
+    if op is not '+' and op is not '-' and \
+       op is not '*' and op is not '/':
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if op is '+':
+        print('{} {} {} = {}'.format(a, op, b, add(a, b)))
+    elif op is '-':
+        print('{} {} {} = {}'.format(a, op, b, sub(a, b)))
+    elif op is '*':
+        print('{} {} {} = {}'.format(a, op, b, mul(a, b)))
+    elif op is '/':
+        print('{} {} {} = {}'.format(a, op, b, div(a, b)))
