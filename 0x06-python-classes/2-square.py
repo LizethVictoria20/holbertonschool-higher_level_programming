5a77412faed22fdf4a87cb6943a1ca4a9d004667
LizethVictoria20 <lizvictoria754@gmail.com>
2020-05-19 21:26:10 +0000

    Update 3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+'''2-square.py: Python script that defines square,
+   private instantiation attribute of size'''
+
+
 class Square:
+    '''Creates an class Square'''
     def __init__(self, size=0):
+        '''Initializes Square with size with zero'''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
