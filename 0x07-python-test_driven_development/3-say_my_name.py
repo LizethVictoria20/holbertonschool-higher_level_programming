e34331c01044c5d82b05c2c06c038970449e9966
LizethVictoria20 <lizvictoria754@gmail.com>
2020-05-22 00:28:42 +0000

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Say my name
+Write a function that prints My name is <first name> <last name>
+"""
+    
+def say_my_name(first_name, last_name=""):
+    """[summary]
+
+    Arguments:
+        first_name {[str]} -- [first name]
+
+    Keyword Arguments:
+        last_name {str} -- [last name] (default: {""})
+    """    
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    
+    print("{} {}".format(first_name, last_name))
