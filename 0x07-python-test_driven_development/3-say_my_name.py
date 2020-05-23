0aa0047adba8accd68f3a226a9c923d875ab9360
LizethVictoria20 <lizvictoria754@gmail.com>
2020-05-23 00:49:28 +0000

    pep8

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,7 +2,8 @@
 """Say my name
 Write a function that prints My name is <first name> <last name>
 """
-    
+
+
 def say_my_name(first_name, last_name=""):
     """[summary]
 
@@ -11,10 +12,10 @@ def say_my_name(first_name, last_name=""):
 
     Keyword Arguments:
         last_name {str} -- [last name] (default: {""})
-    """    
+    """
+
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    
     print("{} {}".format(first_name, last_name))
