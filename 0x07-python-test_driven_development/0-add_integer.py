887daf777c8d7012571aba9724224698da3c0ca2
LizethVictoria20 <lizvictoria754@gmail.com>
2020-05-23 00:45:20 +0000

    Pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,15 +3,18 @@
 a and b must be integers or floats, otherwise raise a TypeError exception
 Returns an integer: the addition of a and b
 """
+
+
 def add_integer(a, b=98):
+
     """[Function add integer]
-        Return the add between a and b 
+        Return the add between a and b
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     elif type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    
+
     if type(a) is float:
         a = int(a)
     elif type(b) is float:
