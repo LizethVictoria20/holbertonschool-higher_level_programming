96bed645099ff8b73db9eaf9cef0a738b405d4df
LizethVictoria20 <lizvictoria754@gmail.com>
2020-06-02 19:51:50 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''[Project about inheritance]
+'''
+
+
+class BaseGeometry:
+    '''[class Geometry module]
+    '''
+    def area(self):
+        '''[function area]
+        '''
+        raise Exception("area() is not implemented")
