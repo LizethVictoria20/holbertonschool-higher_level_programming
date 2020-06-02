e42340bde042083c64d21746328b79621ac126f9
LizethVictoria20 <lizvictoria754@gmail.com>
2020-06-02 23:16:08 +0000

    Update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,7 +10,8 @@ def inherits_from(obj, a_class):
         obj ([int]): [description]
         a_class ([obj]): [description]
     '''
-    if issubclass(a_class, type(obj)):
-        return False
-    else:
-        return True
+    if issubclass(type(obj), a_class):
+        if type(obj) is not a_class:
+            return True
+        else:
+            False
