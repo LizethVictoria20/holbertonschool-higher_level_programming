bfacea13925f2217c9406e7eec624485d6f630dc
LizethVictoria20 <lizvictoria754@gmail.com>
2020-06-02 19:21:06 +0000

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''[Project about inheritance]
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''[object is an instance of, or if the object is an instance of a class]
+
+    Args:
+        obj ([int]): [description]
+        a_class ([obj]): [description]
+    '''
+    return isinstance(obj, a_class)
