bd09c8f5bc485ee45e6433242f8a4eeabfb605cb
LizethVictoria20 <lizvictoria754@gmail.com>
2020-06-03 22:59:11 +0000

    Update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     Args:
         filename (str, file): [description]. Defaults to "".
     '''
-    with open(filename, encoding="utf-8") as File:
-        print(File.read())
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
