aa135744e94406ea6072784fd48a89ebc81c5b32
LizethVictoria20 <lizvictoria754@gmail.com>
2020-06-03 23:24:31 +0000

    Read n lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -4,7 +4,12 @@
 
 
 def read_lines(filename="", nb_lines=0):
-    count = 0
+    '''[read line]
+
+    Args:
+        filename (str, optional): [description]. Defaults to "".
+        nb_lines (int, optional): [description]. Defaults to 0.
+    '''    
     with open(filename, encoding="utf-8") as f:
         if nb_lines > 0:
             for i in range(nb_lines):
