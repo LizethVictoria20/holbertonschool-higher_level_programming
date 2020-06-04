b281a99ea40df7907b1ba52661b07c4bd265b66c
LizethVictoria20 <lizvictoria754@gmail.com>
2020-06-04 16:21:15 +0000

    To JSON string

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -7,4 +7,12 @@ import json
 
 
 def to_json_string(my_obj):
+    '''[to_json_string]
+
+    Args:
+        my_obj ([type]): [description]
+
+    Returns:
+        [json]: [JSON representation]
+    '''
     return json.dumps(my_obj)
