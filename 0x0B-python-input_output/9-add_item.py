0d484b4a0fea1c7ad4af8eddb6518c3855106693
LizethVictoria20 <lizvictoria754@gmail.com>
2020-06-04 18:01:16 +0000

    Load, add, save

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''[read, write files]
+'''
+import json
+import sys
+
+
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+
+file_name = "add_item.json"
+
+try:
+    new = save_to_json_file(file_name) + sys.arvg[1:]
+
+except Exception:
+    new = sys.argv[1:]
+
+save_to_json_file(new, file_name)
