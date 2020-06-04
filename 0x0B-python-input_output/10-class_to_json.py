72895cd0c0b25cda97f9ca36ee6e87872122d9b3
LizethVictoria20 <lizvictoria754@gmail.com>
2020-06-04 18:38:43 +0000

    Class to JSON

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''[read, write files]
+'''
+
+
+def class_to_json(obj):
+    '''[class to json]
+
+    Args:
+        obj ([type]): [description]
+    '''
+    return obj.__dict__
