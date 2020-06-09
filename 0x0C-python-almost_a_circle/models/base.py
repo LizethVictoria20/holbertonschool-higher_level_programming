0b0fd837bd293d132abf0b45b3c0983ac93e1bf4
LizethVictoria20 <lizvictoria754@gmail.com>
2020-06-09 19:19:05 +0000

    Update function to_json_str

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,10 +25,9 @@ class Base:
     def to_json_string(list_dictionaries):
         '''[to json string]
         '''
-        if list_dictionaries is None or len(list_dictionaries) < 0:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
