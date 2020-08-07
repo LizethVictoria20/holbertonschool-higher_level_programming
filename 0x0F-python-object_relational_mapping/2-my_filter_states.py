ef5090daa177e5238ad12f450ebad205a23dd059
LizethVictoria20 <lizvictoria754@gmail.com>
2020-08-07 23:52:42 +0000

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Filter states by user input"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    namedb = sys.argv[3]
+    nameSearched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=username, passwd=password, db=namedb)
+
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states WHERE states.name LIKE "{:s}"
+    ORDER BY states.id ASC""".format(nameSearched))
+    rows = cursor.fetchone()
+
+    while(rows):
+        print(rows)
+        rows = cursor.fetchone()
