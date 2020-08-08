8a8fbdedd75e251011054550f1b9e0bfec5320b2
LizethVictoria20 <lizvictoria754@gmail.com>
2020-08-08 20:09:34 +0000

    Add comment

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,6 +8,10 @@ Base = declarative_base()
 
 
 class State(Base):
+    """State
+    Args:
+        Base ([class])
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
