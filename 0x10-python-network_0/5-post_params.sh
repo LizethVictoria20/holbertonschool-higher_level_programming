ff110f5529339140f7a6ac1eac6ac70a26837c8c
LizethVictoria20 <lizvictoria754@gmail.com>
2020-08-18 20:56:38 +0000

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #cURL POST parameters
-curl --data "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -s --data "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
