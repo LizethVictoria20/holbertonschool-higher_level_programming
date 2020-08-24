ea8c6d20345aad5141662b1a98775bfcda184c0f
LizethVictoria20 <lizvictoria754@gmail.com>
2020-08-24 21:09:35 +0000

    What is my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """What's my status? #0"""
-import urllib.request
+import requests
 
 if __name__ == "__main__":
     """[Function initial]"""
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
 
-    with urllib.request.urlopen(req) as response:
-        value_h = response.read()
-        value_h = value_h.decode('utf-8')
-        print('Body response:')
-        print('\t- type: {}'.format(type(value_h)))
-        print('\t- content: {}'.format(value_h))
+    value = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(value.text)))
+    print('\t- content: {}'.format(value.text))
