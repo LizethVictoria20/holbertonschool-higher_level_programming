9ac909f2e837f607feda9224dbfff6c483ff86b9
LizethVictoria20 <lizvictoria754@gmail.com>
2020-08-24 17:15:52 +0000

    Update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     req = urllib.request.Request('https://intranet.hbtn.io/status')
 
     with urllib.request.urlopen(req) as response:
-        value_headers = response.read()
+        value_h = response.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(value_headers)))
-        print('\t- content: {}'.format(value_headers))
-        print('\t- utf8 content: {}'.format(value_headers.decode('utf-8')))
+        print('\t- type: {}'.format(type(value_h)))
+        print('\t- content: {}'.format(value_h))
+        print('\t- utf8 content: {}'.format(value_h.decode(unicode='utf-8')))
