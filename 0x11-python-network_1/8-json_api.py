9339664e57957a7c1fd93898ac2624ce2c1204ca
LizethVictoria20 <lizvictoria754@gmail.com>
2020-08-24 23:43:54 +0000

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,18 +5,19 @@ import sys
 
 if __name__ == "__main__":
     """[Function initial]"""
-    q = ''
-    if len(q) > 1:
-        q = sys.argv[1]
 
+    if len(sys.argv) >= 2:
+        q = sys.argv[1]
+    else:
+        q = ''
     url = 'http://0.0.0.0:5000/search_user'
     new_param = {'q': q}
     value = requests.post(url, data=new_param)
     try:
         value_json = value.json()
-        if value_json:
-            print("[{}] {}".format(value_json['id'], value_json['name']))
-        else:
+        if len(value_json) == 0:
             print('No result')
+        else:
+            print("[{}] {}".format(value_json['id'], value_json['name']))
     except ValueError:
         print('Not a valid JSON')
