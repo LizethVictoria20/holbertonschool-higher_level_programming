9b613c489a2a5d4f25d932e7ab66ca204e19c8b8
LizethVictoria20 <lizvictoria754@gmail.com>
2020-08-24 21:13:54 +0000

    Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Response header value #1"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    """[Function initial]"""
+    url = sys.argv[1]
+    value = requests.get(url)
+    value_header = value.headers['x-request-id']
+    print(value_header)
