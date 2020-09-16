34cd48ae752983aca8bec8bd1fefdf9d3aac12a1
Liz Victoria Franco <lizvictoria754@gmail.com>
2020-09-16 11:36:23 -0500

    Value of my argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myArgs = process.argv.slice(2);
+
+if (myArgs[0] == null) {
+  console.log('No argument');
+} else {
+  myArgs.forEach((val) => {
+    console.log(`${val}`);
+  });
+}
