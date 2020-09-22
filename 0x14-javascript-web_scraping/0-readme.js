cd1307b057d1b7e7c37aabba799a447c62d9259c
Liz Victoria Franco <lizvictoria754@gmail.com>
2020-09-22 13:27:37 -0500

    Update console

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
+  } else {
+    console.log(data);
   }
-  console.log(data.toString());
 });
