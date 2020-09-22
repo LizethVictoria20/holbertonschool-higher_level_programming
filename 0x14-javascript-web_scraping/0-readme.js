236fbb1c73c9a9af88174ea617945d0577f53251
Liz Victoria Franco <lizvictoria754@gmail.com>
2020-09-22 13:23:20 -0500

    Update return

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     return console.error(err);
   }
-  console.log(data.toString());
+  return console.log(data.toString());
 });
