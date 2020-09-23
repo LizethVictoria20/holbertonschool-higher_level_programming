50a3b853126266442f293ca9fe7e16f694aa0c64
Liz Victoria Franco <lizvictoria754@gmail.com>
2020-09-23 10:42:09 -0500

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+
+const options = {
+  url: url,
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(process.argv[3], url, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    console.log(body);
+  }
+}
+request(options, callback);
