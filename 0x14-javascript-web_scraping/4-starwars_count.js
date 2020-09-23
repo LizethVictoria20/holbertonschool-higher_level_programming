659386de65ced70e3db6e62aafa97f7e5cb167fd
Liz Victoria Franco <lizvictoria754@gmail.com>
2020-09-22 22:55:16 -0500

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const options = {
+  url: url,
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const info = JSON.parse(body);
+    // const url = 'https://swapi-api.hbtn.io/api/people/18/';
+    // let count = 0;
+    // if (info.characters === url) {
+    // count += 1
+    // }
+    let count = 0;
+    for (let i = 0; i < info.results.length; i++) {
+      const allcharacters = info.results[i].characters;
+      const url = 'https://swapi-api.hbtn.io/api/people/18/';
+      for (let j = 0; j < allcharacters.length; j++) {
+        if (url == allcharacters[j]) {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+}
+request(options, callback);
