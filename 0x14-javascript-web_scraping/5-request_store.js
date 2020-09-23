35b818edcff898f6f43c2f72c453ab5b3953f007
Liz Victoria Franco <lizvictoria754@gmail.com>
2020-09-23 10:44:32 -0500

    Update

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -15,9 +15,10 @@ function callback (error, response, body) {
     fs.writeFile(process.argv[3], url, 'utf-8', (err) => {
       if (err) {
         console.log(err);
+      } else {
+        console.log(body);
       }
     });
-    console.log(body);
   }
 }
 request(options, callback);
