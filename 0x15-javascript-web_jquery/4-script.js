fcdf31c420db784b56335622a927322812efbcac
Liz Victoria Franco <lizvictoria754@gmail.com>
2020-09-24 22:41:16 -0500

    Update function toggle

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,8 +1,4 @@
 $('div#toggle_header').click(() => {
-  $('header').addClass('red');
-  if ($('header').hasClass('red')) {
-    $('header').toggleClass('green');
-  } else {
-    $('header').toggleClass('red');
-  }
+  $('header').toggleClass('red');
+  $('header').toggleClass('green');
 });
